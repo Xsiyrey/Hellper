a28982da8abdd908a084d25140d0b4f4106607b3
Slava Kurganov <sgorelchik@mail.ru>
2019-07-02 17:13:10 +0300

    Создан класс работы с файлами
    Добавлен алгоритм получения файлов из каталога и подкаталогов

---
+++ Hellper/Algorithm/FileWorker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellper.Algorithm
+{
+    class FileWorker
+    {
+        /// <summary>
+        /// Получение файлов из каталога и подкаталогов
+        /// </summary>
+        /// <param name="path">Путь</param>
+        /// <param name="searchPattern">Паттерн</param>
+        /// <returns></returns>
+        private IEnumerable<string> FindFilesPath(string path, string searchPattern)
+        {
+            string[] files;
+            try
+            {
+                files = Directory.GetFiles(path, searchPattern);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                yield break;
+            }
+
+            foreach (string file in files)
+            {
+                yield return file;
+            }
+
+            string[] directories;
+            try
+            {
+                directories = Directory.GetDirectories(path);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                yield break;
+            }
+
+            foreach (string subdirectory in directories)
+            {
+                foreach (string file in FindFilesPath(subdirectory, searchPattern))
+                {
+                    yield return file;
+                }
+            }
+        }
+    }
+}
